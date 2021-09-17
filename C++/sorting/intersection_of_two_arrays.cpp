81baff8fa767406c88b73bb56df1b8dcb8977529
Dhivya S <dhivyasreedhar@gmail.com>
2021-09-17 10:48:05 +0530

    Create intersection_of_two_arrays.cpp

---
+++ C++/sorting/intersection_of_two_arrays.cpp
@@ -0,0 +1,27 @@
+blic:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        vector<int> ans;
+        int i=0,j=0,n=nums1.size(),m=nums2.size();
+        while(i<n&&j<m)
+        {
+            if(nums1[i]==nums2[j])
+            { ans.push_back(nums1[i]);
+            while(i<n-1&&nums1[i]==nums1[i+1])
+                i++;
+             while(j<m-1&&nums2[j]==nums2[j+1])j++;
+             i++;
+             j++;
+                
+             }
+            else if(nums1[i]>nums2[j])
+                j++;
+            else
+                i++;
+        }
+        
+        return ans;
+        
+    }
+};
