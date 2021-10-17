b16173c2e803363f47637561bc5aef91fe50c985
Dhivya S <dhivyasreedhar@gmail.com>
2021-10-17 17:52:59 +0530

    Create 1342. Number of Steps to Reduce a Number to Zero.cpp

---
+++ C++/Math/1342. Number of Steps to Reduce a Number to Zero.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numberOfSteps(int num) {
+        int s=0;
+        while(num)
+        {
+            if(num%2)
+            {num-=1;s++;}
+            else
+            {
+                num/=2;
+                s++;
+            }
+        }
+        return s;
+        
+    }
+};
